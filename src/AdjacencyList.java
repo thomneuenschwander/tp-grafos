acdd5dc373c2e1e96eb4c8ae9d60148b87935eef
thomneuenschwander <thom.nmaciel.baron@gmail.com>
2024-09-16 20:39:15 -0300

    has two internally disjoint paths prototype

--- src/AdjacencyList.java
+++ src/AdjacencyList.java
@@ -1,7 +1,9 @@
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Grafo nao direcionado representado por uma Lista de Adjacência
@@ -18,7 +20,8 @@ import java.util.Map;
 public class AdjacencyList implements UndirectedGraph {
     private Map<Integer, List<Integer>> graph = new HashMap<>();
 
-    public AdjacencyList() { }
+    public AdjacencyList() {
+    }
 
     @Override
     public List<Integer> getNeighborhood(int v) {
@@ -45,28 +48,64 @@ public class AdjacencyList implements UndirectedGraph {
 
     /*
      * 
-     * Método que verifique a existência de dois caminhos internamente disjuntos 
+     * Verifica a existência de dois caminhos internamente disjuntos
      * (ou um ciclo) entre cada par de vértices do bloco
      * 
      */
     public boolean hasTwoInternallyDisjointPaths(int v, int w) {
-        var nbhdV = getNeighborhood(v);
-        var nbhdW = getNeighborhood(w);
-
-        if(nbhdV.size() < 2 || nbhdW.size() < 2)
+        if (getNeighborhood(v).size() < 2 || getNeighborhood(w).size() < 2)
             return false;
 
-        
+        int[] disc = new int[N()];
+        int[] spawningTree = new int[N()];
+        var count = new AtomicInteger();
 
-        return true;
+        for (int i = 0; i < N(); i++) {
+            disc[i] = 0;
+            spawningTree[i] = -1;
+        }
+
+        return performDFS(v, w, disc, count, spawningTree, v);
     }
 
+    private boolean performDFS(int v, int w, int[] disc, AtomicInteger count, int[] pred, int currentV) {
+        disc[currentV - 1] = count.incrementAndGet();
+
+        var neighborhood = getNeighborhood(currentV);
+        neighborhood.sort(Comparator.naturalOrder());
+
+        for (int u : neighborhood) {
+            if (disc[u - 1] == 0) {
+                pred[u - 1] = currentV;
+                performDFS(v, w, disc, count, pred, u);
+
+            } else if (u != pred[currentV - 1] && currentV < u) {
+                int i = (Math.max(disc[u - 1], disc[v - 1]) == disc[u - 1]) ? u : v;
+                boolean containsV = false;
+                boolean containsW = false;
+
+                while (!(i == -1)) {
+                    if(i == v)
+                        containsV = true;
+                    if(i == w)
+                        containsW = true;
+
+                    if(containsV && containsW)
+                        return true;
+
+                    i = pred[i - 1];
+                }
+            }
+
+        }
+
+        return false;
+    }
 
     @Override
     public int N() {
         return graph.size();
     }
-
 
     @Override
     public int M() {
@@ -81,11 +120,12 @@ public class AdjacencyList implements UndirectedGraph {
     /*
      * Undirected Graph Format:
      * 
-     *  N M        // n -> number of vertices  M -> number of edges  
-     *  v w       // Next M lines -> Undirected edge {v, w} that touches 2 vertices v and w.
-     *  v w              
-     * (...)         
-    */
+     * N M // n -> number of vertices M -> number of edges
+     * v w // Next M lines -> Undirected edge {v, w} that touches 2 vertices v and
+     * w.
+     * v w
+     * (...)
+     */
     public static AdjacencyList readGraphFile(String filePath) {
         var adjList = new AdjacencyList();
         UndirectedGraph.readGraphFile(filePath, adjList);
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
-    static final String GRAPH_FILE_PATH = "graph-files/graph_test.txt";
+    static final String GRAPH_FILE_PATH = "graph-files/small/simple-conectected.txt";
 
     public static void main(String[] args) {
         AdjacencyList graph = AdjacencyList.readGraphFile(GRAPH_FILE_PATH);
+
+        System.out.println(graph.hasTwoInternallyDisjointPaths(1, 4));
         
         System.out.println(graph);
     }
